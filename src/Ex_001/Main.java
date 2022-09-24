628004611fcb5a7f0d83f3fc1e24d2d584c17a23
DJ-UKOL <dj-ukol@yandex.ru>
2022-09-24 13:31:10 +0500

    Init

---
+++ src/Ex_001/Main.java
@@ -0,0 +1,10 @@
+package Ex_001;
+/*
+ * https://javarush.ru/groups/posts/1941-kak-ne-poterjatjhsja-vo-vremeni--datetime-i-calendar
+*/
+public class Main {
+    public static void main(String[] args) {
+        String date = "11 июня 2018 года";
+        System.out.println(date);
+    }
+}
